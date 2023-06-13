4a9902d9b31cb35445c741e79654c379dfb7a990
j30ngwoo <ttangle1234@naver.com>
2023-06-13 16:23:21 +0900

    no change

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -19,6 +19,7 @@ export default function RegisterForm(){
 			"password": {pw}
 		}).then((event) => {
       console.log('received', event);
+      
 			//localStorage.setItem("id", event);
       // setLogin(true);
       // setNickname('jeongwoo');
